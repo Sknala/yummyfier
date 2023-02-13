2bdf6659c6ef02aa12779ee00c6c145b025efaed
Iiris <iiris-harju@hotmail.com>
2023-02-13 14:46:04 +0200

    Index page started

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,11 @@
 // Test components folder and file
 import React from "react";
+import './index.css';
 
 export default function Test() {
     return (
         <div>
-            Hello
+            <h1 className="header">Yummyfier.</h1>
         </div>
     );
 
