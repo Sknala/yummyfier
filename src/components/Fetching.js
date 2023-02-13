2038af184a510febef70846af5f03d0b6debbbd6
Iiris <iiris-harju@hotmail.com>
2023-02-13 13:09:09 +0200

    Test for meals strating with b

--- src/components/Fetching.js
+++ src/components/Fetching.js
@@ -1,23 +1,23 @@
-import React, { useEffect} from "react";
+import React, { useEffect } from "react";
 
 
-export default function Fetching () {
+export default function Fetching() {
 
 
   //Fetching meals filtered by first letter ex: f=a, f=b.
-    useEffect(() => {
-        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=a`)
-         .then((response) => response.json())
-         .then((data) => console.log(data))
-         .catch((err) => {
-          console.log(err.message);
-         });
-       }, []);
+  useEffect(() => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=b`)
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
 
-       return (
-<div>
-  
-</div>
-       );
+  return (
+    <div>
+
+    </div>
+  );
 
 }
