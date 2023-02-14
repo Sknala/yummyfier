75921432e7238b82fa58249b5af1562fd9b64ffb
Iiris <iiris-harju@hotmail.com>
2023-02-14 09:00:15 +0200

    Header style

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,12 @@
 // Test components folder and file
-import React from "react";
+import React, { useState } from 'react';
 import './index.css';
 
 export default function Index() {
     return (
         <div className="container">
-            <div className="headerbox">
-                <h1 className="header">Yummyfier.</h1>
+            <div className="header">
+                Yummyfier.
             </div>
         </div>
     );
