fca4f280b1193986efffdfbe916eb65f6f041508
Iiris <iiris-harju@hotmail.com>
2023-03-06 16:46:42 +0200

    header, create user and log in put together

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,14 @@
 // Test components folder and file
 import React, { useState } from 'react';
 import './header.css';
+import LogIn from './logIn';
+import CreateUser from './createUser';
 
 export default function Header() {
     return (
         <div className="container">
+            <LogIn />
+            <CreateUser />
             <div className="header">
                 Yummyfier.
             </div>
