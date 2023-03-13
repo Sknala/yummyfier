09cde8399fda469c68d98148deb28a036923e046
jjenska <jenni.erkkila@myy.haaga-helia.fi>
2023-03-13 14:35:49 +0200

    merge fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-import Footer from './components/Footer';
-import Header from './components/header';
-import Slogan from './components/slogan';
-=======
 import Footer from "./components/Footer";
 import Header from "./components/header";
->>>>>>> edamam/fetch
+import Slogan from "./components/slogan";
 
 function App() {
   return (
