42d8cb375ca2dfc18294803b95de9a60e6ca8ed3
cisse23 <baker.cisse@gmail.com>
2023-03-27 14:34:00 +0300

    added temporary button to toggle slogan visibility

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import Footer from './components/Footer';
 import Header from './components/header';
 import RecipeFetch from './components/recipeSearch';
 import Slogan from './components/slogan';
-
+import AppContext from './AppContext';
+import { useState } from 'react';
+import { Button } from '@mui/material';
 
 function App() {
+
+  const [showSlogan, setShowSlogan] = useState(true);
+
+
   return (
     <div className="App">
-      <Header />
-      <Slogan />
-      <RecipeFetch />
-      <Footer />
+      <AppContext.Provider value={showSlogan}>
+        <Header />
+        <Button
+          onClick={() => setShowSlogan(!showSlogan)}
+        >
+          Toggle slogan
+        </Button>
+        {showSlogan && <Slogan />}
+        <RecipeFetch />
+        <Footer />
+      </AppContext.Provider>
     </div>
   );
 }
