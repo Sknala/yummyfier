64a4e414afbf07189a198088f98596ccf8299031
verneriheikkila <verneri.heikkila@gmail.com>
2023-04-21 11:22:59 +0300

    RapidAPI integrated taken in use and integrated with main branch

--- src/components/recipeCard.js
+++ src/components/recipeCard.js
@@ -1,91 +1,113 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import {
-  CardActions,
-  IconButton,
-  Card,
-  CardContent,
-  CardMedia,
-  Typography,
-  CardActionArea,
-} from "@mui/material";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import RecipeDialog from "./recipeDialog";
+    CardActions,
+    IconButton,
+    Card,
+    CardContent,
+    CardMedia,
+    Typography,
+    CardActionArea,
+} from '@mui/material';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import RecipeDialog from './recipeDialog';
 
-import "../styles/recipe.css";
+import '../styles/recipe.css';
+
+const API_KEY = process.env.REACT_APP_API_KEY_SPOONACULAR;
 
 function RecipeCard(props) {
-  const { label, image, ingredients, totalTime, calories, url, source } =
-    props.data;
+    const [recipeDetails, setRecipeDetails] = useState(null);
 
-  const convertPrepTime = (totalTime) => {
-    const time = parseInt(totalTime);
+    const { id, title } = props.data;
 
-    if (time === 0) {
-      return ` `;
-    } else if (time > 60) {
-      const hours = Math.floor(time / 60);
-      const minutes = time % 60;
+    useEffect(() => {
+        const fetchRecipeDetails = async () => {
+            try {
+                const response = await fetch(
+                    `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${id}/information`,
+                    {
+                        headers: {
+                            'X-RapidAPI-Host':
+                                'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+                            'X-RapidAPI-Key': API_KEY,
+                        },
+                    }
+                );
+                if (!response.ok) {
+                    throw new Error(`HTTP error: status is ${response.status}`);
+                }
+                const data = await response.json();
+                setRecipeDetails(data);
+            } catch (err) {
+                console.error(err.message);
+            }
+        };
 
-      if (time % 60 === 0) {
-        return `${hours}h`;
-      }
-      return `${hours}h ${minutes} min`;
-    } else {
-      return `${time} min`;
+        fetchRecipeDetails();
+    }, [id]);
+
+    if (!recipeDetails) {
+        return <div></div>;
     }
-  };
 
-  // See Card styles in recipe.css
-  return (
-    <>
-      <Card className="card" >
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            image={image}
-            alt="recipeImage"
-          />
-          <CardContent className="cardContent" >
-            <Typography
-              gutterBottom
-              variant="p"
-              component="div"
-              style={{ fontSize: 18, fontWeight: "bold" }}
-            >
-              {label}
-            </Typography>
-            <Typography variant="body2" color="text.secondary" component="div">
-              <p>Calories: {Math.round(calories)}</p>
-              <ul style={{ padding: 10 }}>
-                {ingredients.slice(0, 5).map((ingredient, index) => (
-                  <li key={index}>{ingredient.text}</li>
-                ))}
-              </ul>
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <RecipeDialog
-            label={label}
-            ingredients={ingredients}
-            source={source}
-            url={url}
-          />
-          <Typography style={{ marginLeft: "auto" }}>
-            {convertPrepTime(totalTime)}
-          </Typography>
-          <IconButton
-            style={{ color: "red", marginLeft: "auto" }}
-            onClick={() => {
-              console.log("Icon Button clicked");
-            }}
-          >
-            <FavoriteIcon />
-          </IconButton>
-        </CardActions>
-      </Card >
-    </>
-  );
+    const { servings, readyInMinutes, extendedIngredients } = recipeDetails;
+
+    // See Card styles in recipe.css
+    return (
+        <>
+            <Card className="card">
+                <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        height="140"
+                        image={recipeDetails.image}
+                        alt="recipeImage"
+                    />
+                    <CardContent className="cardContent">
+                        <Typography
+                            gutterBottom
+                            variant="p"
+                            component="div"
+                            style={{ fontSize: 18, fontWeight: 'bold' }}
+                        >
+                            {title}
+                        </Typography>
+                        <Typography
+                            variant="body2"
+                            color="text.secondary"
+                            component="div"
+                        >
+                            <p>Servings: {servings}</p>
+                            <h4>Ingredients:</h4>
+                            <ul style={{ padding: 10 }}>
+                                {extendedIngredients
+                                    .slice(0, 5)
+                                    .map((ingredient, index) => (
+                                        <li key={index}>
+                                            {ingredient.amount}{' '}
+                                            {ingredient.unit} {ingredient.name}
+                                        </li>
+                                    ))}
+                            </ul>
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <RecipeDialog data={recipeDetails} />
+                    <Typography style={{ marginLeft: 'auto' }}>
+                        {readyInMinutes} mins
+                    </Typography>
+                    <IconButton
+                        style={{ color: 'red', marginLeft: 'auto' }}
+                        onClick={() => {
+                            console.log('Icon Button clicked');
+                        }}
+                    >
+                        <FavoriteIcon />
+                    </IconButton>
+                </CardActions>
+            </Card>
+        </>
+    );
 }
 export default RecipeCard;
