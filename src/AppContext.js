56415f496d3fd42ce1963b953658caa6ab9eff7b
cisse23 <baker.cisse@gmail.com>
2023-04-24 09:47:14 +0300

    appcontext.js lisätty

---
+++ src/AppContext.js
@@ -0,0 +1,24 @@
+//this halped https://dev.to/clickpesa/react-manage-state-using-context-api-with-usestate-or-usereducer-hooks-d5l
+
+import React, { createContext, useContext, useState } from "react"
+
+export const ToggleSloganContext = createContext();
+
+export const ToggleSloganProvider = ({ children }) => {
+    // the value passed in here will be accessible anywhere in our application 
+    // you can pass any value, in our case we pass our state and it's update method 
+    const [data, setData] = useState({
+        showSlogan: true,
+    });
+    
+    return(
+        <ToggleSloganContext.Provider value={{data, setData}}>
+            {children}
+        </ToggleSloganContext.Provider>
+    );
+};
+
+export const useToggleSloganContext = () => useContext(ToggleSloganContext);
+// useToggleContext will be used to use and update state accross the app
+// we can access to data and setData using this method 
+// anywhere in any component that's inside ToggleProvider
