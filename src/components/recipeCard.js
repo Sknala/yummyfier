2bf9017f433d157e1d31d87d3ad82547b264a2ff
jjenska <jenni.erkkila@myy.haaga-helia.fi>
2023-04-28 10:47:41 +0300

    preptime convertion, amount decimal fix

--- src/components/recipeCard.js
+++ src/components/recipeCard.js
@@ -1,113 +1,111 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
-    CardActions,
-    IconButton,
-    Card,
-    CardContent,
-    CardMedia,
-    Typography,
-    CardActionArea,
-} from '@mui/material';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import RecipeDialog from './recipeDialog';
+  CardActions,
+  IconButton,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  CardActionArea,
+} from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import RecipeDialog from "./recipeDialog";
+import FormatPrepTime from "./formatPrepTime";
 
-import '../styles/recipe.css';
+import "../styles/recipe.css";
 
 const API_KEY = process.env.REACT_APP_API_KEY_SPOONACULAR;
 
 function RecipeCard(props) {
-    const [recipeDetails, setRecipeDetails] = useState(null);
+  const [recipeDetails, setRecipeDetails] = useState(null);
 
-    const { id, title } = props.data;
+  const { id, title } = props.data;
 
-    useEffect(() => {
-        const fetchRecipeDetails = async () => {
-            try {
-                const response = await fetch(
-                    `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${id}/information`,
-                    {
-                        headers: {
-                            'X-RapidAPI-Host':
-                                'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
-                            'X-RapidAPI-Key': API_KEY,
-                        },
-                    }
-                );
-                if (!response.ok) {
-                    throw new Error(`HTTP error: status is ${response.status}`);
-                }
-                const data = await response.json();
-                setRecipeDetails(data);
-            } catch (err) {
-                console.error(err.message);
-            }
-        };
+  useEffect(() => {
+    const fetchRecipeDetails = async () => {
+      try {
+        const response = await fetch(
+          `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${id}/information`,
+          {
+            headers: {
+              "X-RapidAPI-Host":
+                "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+              "X-RapidAPI-Key": API_KEY,
+            },
+          }
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error: status is ${response.status}`);
+        }
+        const data = await response.json();
+        setRecipeDetails(data);
+      } catch (err) {
+        console.error(err.message);
+      }
+    };
 
-        fetchRecipeDetails();
-    }, [id]);
+    fetchRecipeDetails();
+  }, [id]);
 
-    if (!recipeDetails) {
-        return <div></div>;
-    }
+  if (!recipeDetails) {
+    return <div></div>;
+  }
 
-    const { servings, readyInMinutes, extendedIngredients } = recipeDetails;
+  const { servings, readyInMinutes, extendedIngredients } = recipeDetails;
 
-    // See Card styles in recipe.css
-    return (
-        <>
-            <Card className="card">
-                <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image={recipeDetails.image}
-                        alt="recipeImage"
-                    />
-                    <CardContent className="cardContent">
-                        <Typography
-                            gutterBottom
-                            variant="p"
-                            component="div"
-                            style={{ fontSize: 18, fontWeight: 'bold' }}
-                        >
-                            {title}
-                        </Typography>
-                        <Typography
-                            variant="body2"
-                            color="text.secondary"
-                            component="div"
-                        >
-                            <p>Servings: {servings}</p>
-                            <h4>Ingredients:</h4>
-                            <ul style={{ padding: 10 }}>
-                                {extendedIngredients
-                                    .slice(0, 5)
-                                    .map((ingredient, index) => (
-                                        <li key={index}>
-                                            {ingredient.amount}{' '}
-                                            {ingredient.unit} {ingredient.name}
-                                        </li>
-                                    ))}
-                            </ul>
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-                <CardActions>
-                    <RecipeDialog data={recipeDetails} />
-                    <Typography style={{ marginLeft: 'auto' }}>
-                        {readyInMinutes} mins
-                    </Typography>
-                    <IconButton
-                        style={{ color: 'red', marginLeft: 'auto' }}
-                        onClick={() => {
-                            console.log('Icon Button clicked');
-                        }}
-                    >
-                        <FavoriteIcon />
-                    </IconButton>
-                </CardActions>
-            </Card>
-        </>
-    );
+  // See Card styles in recipe.css
+  return (
+    <>
+      <Card className="card">
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image={recipeDetails.image}
+            alt="recipeImage"
+          />
+          <CardContent className="cardContent">
+            <Typography
+              gutterBottom
+              variant="p"
+              component="div"
+              style={{ fontSize: 18, fontWeight: "bold" }}
+            >
+              {title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary" component="div">
+              <p>Servings: {servings}</p>
+              <h4 style={{ margin: 0, padding: 0 }}>Ingredients:</h4>
+              <ul style={{ marginLeft: 0, paddingLeft: 20 }}>
+                {extendedIngredients.slice(0, 5).map((ingredient, index) => (
+                  <li key={index}>
+                    {ingredient.amount % 1 === 0
+                      ? ingredient.amount
+                      : ingredient.amount.toFixed(1)}{" "}
+                    {ingredient.unit} {ingredient.name}
+                  </li>
+                ))}
+              </ul>
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <RecipeDialog data={recipeDetails} />
+          <Typography style={{ marginLeft: "auto" }}>
+            {/* Formatting over 60 minutes preparation time to hours and minutes */}
+            {FormatPrepTime({ readyInMinutes })}
+          </Typography>
+          <IconButton
+            style={{ color: "red", marginLeft: "auto" }}
+            onClick={() => {
+              console.log("Icon Button clicked");
+            }}
+          >
+            <FavoriteIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
+    </>
+  );
 }
 export default RecipeCard;
--- src/components/recipeDialog.js
+++ src/components/recipeDialog.js
@@ -1,83 +1,82 @@
-import React, { useState } from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import { Link } from '@mui/material';
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { Link } from "@mui/material";
 
 function RecipeDialog(props) {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    const { title, extendedIngredients, readyInMinutes, servings, sourceUrl } =
-        props.data;
+  const { title, extendedIngredients, readyInMinutes, servings, sourceUrl } =
+    props.data;
 
-    const instructions =
-        props.data.analyzedInstructions &&
-        props.data.analyzedInstructions.length > 0 &&
-        props.data.analyzedInstructions[0].steps
-            ? props.data.analyzedInstructions[0].steps
-            : [];
+  const instructions =
+    props.data.analyzedInstructions &&
+    props.data.analyzedInstructions.length > 0 &&
+    props.data.analyzedInstructions[0].steps
+      ? props.data.analyzedInstructions[0].steps
+      : [];
 
-    return (
-        <div>
-            <Button size="small" variant="text" onClick={handleClickOpen}>
-                Click to see more
-            </Button>
-            <Dialog open={open} onClose={handleClose}>
-                <DialogTitle>{title}</DialogTitle>
-                <DialogContent>
-                    <DialogContentText component="div">
-                        <p>Preparation time: {readyInMinutes} minutes</p>
-                        <p>Servings: {servings}</p>
-                        <h4>Ingredients:</h4>
-                        <ul style={{ padding: 10 }}>
-                            {extendedIngredients.map((ingredient, index) => (
-                                <li key={index}>
-                                    {ingredient.amount} {ingredient.unit}{' '}
-                                    {ingredient.name}
-                                </li>
-                            ))}
-                        </ul>
-                        <h4>Instructions:</h4>
-                        <ol>
-                            {instructions.map((step, index) => (
-                                <li
-                                    key={index}
-                                    dangerouslySetInnerHTML={{
-                                        __html: step.step,
-                                    }}
-                                />
-                            ))}
-                        </ol>
-                        <p>
-                            Full recipe on{' '}
-                            <Link
-                                style={{ textDecoration: 'none' }}
-                                href={sourceUrl}
-                            >
-                                {sourceUrl}
-                            </Link>
-                        </p>
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button color="error" onClick={handleClose}>
-                        Close
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        </div>
-    );
+  return (
+    <div>
+      <Button size="small" variant="text" onClick={handleClickOpen}>
+        Click to see more
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>{title}</DialogTitle>
+        <DialogContent>
+          <DialogContentText component="div">
+            <p>Preparation time: {readyInMinutes} minutes</p>
+            <p>Servings: {servings}</p>
+            <h4>Ingredients:</h4>
+            <ul style={{ padding: 10 }}>
+              {extendedIngredients.map((ingredient, index) => (
+                <li key={index}>
+                  {ingredient.amount % 1 === 0
+                    ? ingredient.amount
+                    : ingredient.amount.toFixed(1)}{" "}
+                  {ingredient.unit} {ingredient.name}
+                </li>
+              ))}
+            </ul>
+            <h4>Instructions:</h4>
+            <ol>
+              {instructions.map((step, index) => (
+                <li
+                  key={index}
+                  dangerouslySetInnerHTML={{
+                    __html: step.step,
+                  }}
+                />
+              ))}
+            </ol>
+            <p>
+              Full recipe on{" "}
+              <Link style={{ textDecoration: "none" }} href={sourceUrl}>
+                {sourceUrl}
+              </Link>
+            </p>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button color="error" onClick={handleClose}>
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 }
 
 export default RecipeDialog;
